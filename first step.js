eaf9efc4dba6e304ffef916a8cd1ca859a5d2d6c
Wilfred <wilfredgeorgem@gmail.com>
2021-03-13 18:15:09 +0530

    first step file added

---
+++ first step.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asr2'
+        },
+        {
+          name: 'Zombie',
+          id: 'as1w'
+        }
+      ]
+    };
+  }
+  render() {
+    return(
+      <div className="App">
+            {this.state.monsters.map(monster => (<h1 key={monster.id}>npn { monster.name} </h1>))}
+          </div>
+        );
+      }
+     }
+
+
+export default App;

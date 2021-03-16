a69e9b795d75ff02d768515791e83baf24c46723
Wilfred <wilfredgeorgem@gmail.com>
2021-03-16 18:26:10 +0530

    cart-list trial added

--- card-list trial.js
+++ card-list trial.js
@@ -7,24 +7,23 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
       monsters: []
-    };
+
+      };
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
+    .then(Response => Response.json())
     .then(users => this.setState({ monsters: users}));
   }
-    
   render() {
     return(
-     <div className="App">
-       <CardList name = 'Yihua'>
-        {this.state.monsters.map(monster => (<h1 key={monster.id}> { monster.name} </h1>))}
-        </CardList> 
-        </div>
+      <div className="App">
+        <CardList name = 'Yihua'>
+        {this.state.monsters.map(monster => (<h1 key={monster.id}>{ monster.name} </h1>))}  
+        </CardList>
+            </div>
         );
       }
      }
